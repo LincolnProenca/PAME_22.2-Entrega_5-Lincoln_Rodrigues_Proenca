d5711122b7300e9a14ec6ef9ea0b6c5fa3a4fc23
LincolnProenca <lincoln22220@gmail.com>
2023-01-26 00:53:56 -0300

    Criando arquivo de rotas separado

--- sistema-consultas/src/App.js
+++ sistema-consultas/src/App.js
@@ -1,19 +1,10 @@
 import { GlobalStyle } from "./Constants/theme";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home";
-import Agendamento from "./Pages/Agendamento";
-import Gerenciamento from "./Pages/Gerenciamento";
+import Rotas from "./Routes";
 
 function App() {
   return (
     <>
-      <BrowserRouter className="App">
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/agendamento" element={<Agendamento />} />
-          <Route exact path="/gerenciamento" element={<Gerenciamento />} />
-        </Routes>
-      </BrowserRouter>
+      <Rotas></Rotas>
       <GlobalStyle></GlobalStyle>
     </>
   );
